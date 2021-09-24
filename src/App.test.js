a1c0e801879c2a092ac1852a7c28a52731feae15
funduleadiana <diana.fundulea@yahoo.com>
2021-09-24 14:39:16 +0100

    additional testing fireEvent changes color and style

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -6,6 +6,13 @@ test('renders learn react link', () => {
   //testing for initial red button
   const buttonColor = screen.getByRole('button', {name: 'Change to blue'});
   //expect the background color to be red
-  
+
   expect(buttonColor).toHaveStyle({backgroundColor: 'red'})
+  //expecting a click event on the button
+  fireEvent.click(buttonColor)
+  //after the click event we expect the color to change to blue
+  expect(buttonColor).toHaveStyle({backgroundColor: 'blue'});
+
+  //expecting to have the correct text after the color has changed
+  expect(buttonColor.textContent).toBe('Change to red');
 });
