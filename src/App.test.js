fc2a96a304d5783c82302bdadbe3b2ad5d8c6b5e
funduleadiana <diana.fundulea@yahoo.com>
2021-09-24 14:31:16 +0100

    setting up intital test for a display of a button with color red

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //testing for initial red button
+  const buttonColor = screen.getByRole('button', {name: 'Change to blue'});
+  //expect the background color to be red
+  
+  expect(buttonColor).toHaveStyle({backgroundColor: 'red'})
 });
