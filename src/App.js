25ea7e63240e25bfae2a547e6d0f3a82662c23c3
funduleadiana <diana.fundulea@yahoo.com>
2021-09-29 12:24:17 +0100

    adding the react code for the checkBox and the disabled button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor ] = useState('red')
+  const [disabled, setDisabled] = useState(false);
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
   return (
     <div className="App">
-      <button style={{backgroundColor: buttonColor}} onClick={()=> setButtonColor(newButtonColor)}>Change to {newButtonColor}</button>
+      <button style={{backgroundColor: buttonColor}} disabled={disabled} onClick={()=> setButtonColor(newButtonColor)}>Change to {newButtonColor}</button>
       <br/>
-      <input type="checkbox"/>
+      <input type="checkbox" id="enable-button" 
+      //defaulting to the default disabled 
+      defaultChecked={disabled} 
+      //Accessibility issues
+      aria-checked={disabled}
+      onChange={(e)=> setDisabled(e.target.checked)}/>
       </div>
   );
 }
